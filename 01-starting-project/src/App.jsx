b4b1421be5f9c197637aef28249841d5a0d653f0
claudiney63 <claudineyryan75@gmail.com>
2024-03-20 06:42:59 -0300

    Using & Outputting Dynamic Values [Core Concept]

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,19 @@
+const description = ["Fundamental", "Core", "Crucial", "Essential"];
+
+function randomDescription(max) {
+  return Math.floor(Math.random() * max + 1);
+}
+
 function HeaderComponent() {
+  const descriptionConst = description[randomDescription(3)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {descriptionConst} React concepts you will need for almost any app you
+        are going to build!
       </p>
     </header>
   );
