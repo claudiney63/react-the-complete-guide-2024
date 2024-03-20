88821fd0a55e00a1027b9e6892856c34b1d245fb
claudiney63 <claudineyryan75@gmail.com>
2024-03-20 06:47:53 -0300

    Setting HTML Attributes Dynamically & Loading Image Files

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import logoReact from './assets/react-core-concepts.png'
+
 const description = ["Fundamental", "Core", "Crucial", "Essential"];
 
 function randomDescription(max) {
@@ -9,7 +11,7 @@ function HeaderComponent() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={logoReact} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {descriptionConst} React concepts you will need for almost any app you
